319f538f1ac19a593324f542261f1299764f9c58
mihaialin <mihaialin@wildmedia.ro>
2014-01-17 15:52:48 +0200

    m

--- WebGail/Contact.aspx.cs
+++ WebGail/Contact.aspx.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Net;
+using System.IO;
 using MySql.Data.MySqlClient;
 
 
@@ -58,6 +59,25 @@ namespace WebGail
             cmd.Parameters.AddWithValue("@lifeResult", currentUser.lifeResult);
 
             cmd.ExecuteNonQuery();
+
+            WebRequest request = WebRequest.Create("http://mapi.frenq.com/sendmail.php?email=" + currentUser.email);
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
         }
 
         public class dbEntry {
